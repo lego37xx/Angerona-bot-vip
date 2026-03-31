bb3ee696310e076ffeffd589868eb8e0115c8e49
lego37xx <lego37@hotmail.com>
2026-03-31 14:09:13 -0600

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from telegram import Update
 from telegram.ext import Application, CommandHandler, MessageHandler, filters, ContextTypes
 
 # --- ⚙️ CONFIGURACIÓN CRÍTICA ---
-TOKEN = '8616684285:AAEF271duWSPV0-UfJ9rK4rUdI6UQz1-Zxg'
+TOKEN = '8616684285:AAFyPoA2cKvB1lDVONjH49iOtBDUMzn6L1k'
 DUENO_ID = 8650569384
 ID_GRUPO_FIJO = -1003519088233
 NOMBRE_FOTO = 'logo.png'
